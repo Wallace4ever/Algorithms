cc1382ce42a39cafb8599f636e60560f4de3ec1e
Wallace <xpkllp@hotmail.com>
2020-03-18 09:38:10 +0800

    Self designed HashSet, with compilation passed but excution error.

--- src/edu/seu/ListNode.java
+++ src/edu/seu/ListNode.java
@@ -1,13 +1,14 @@
 package edu.seu;
-
+import java.util.*;
 public class ListNode {
      int val;
      ListNode next;
      ListNode(int x) { val = x; }
+     ListNode() { }
 }
 
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {//两数之和
         int jinwei=0;
         ListNode result=new ListNode(0);
         ListNode p=result;
@@ -39,5 +40,132 @@ class Solution {
             }
         }
         return result;
+    }
+
+    public static int removeElement(int[] nums, int val) {//移除元素
+        int i=0;
+        for(int j=0;j<nums.length;j++){
+            while(nums[j]==val){
+                if (j==nums.length-1){return i;}
+                j+=1;
+            }
+            nums[i]=nums[j];
+            i++;
+        }
+        return i;
+    }
+
+    public int[] plusOne(int[] digits) {//加一
+        for(int i=digits.length-1;i>=0;i--){
+            digits[i]+=1;
+            if(digits[i]==10){
+                digits[i]=0;
+                continue;
+            }else{return digits;}
+        }
+        int[] res=new int[digits.length+1];
+        res[0]=1;
+        for(int i=1;i<=digits.length;i++){
+            res[i]=digits[i-1];
+        }
+        return res;
+    }
+
+    public boolean hasCycle(ListNode head) {//环形链表
+        if (head==null){return false;}
+        ListNode p1=head;
+        ListNode p2=head;
+        while(p1.next!=null){
+            if(p2.next==null||p2.next.next==null){return false;}
+            p1=p1.next;
+            p2=p2.next.next;
+            if(p1==p2){return true;}
+        }
+        return false;
+    }
+
+    public ListNode detectCycle(ListNode head) {//环形链表II
+        if (head==null){return null;}
+        ListNode p1=head;
+        HashSet<ListNode> index=new HashSet<ListNode>();
+        while(p1.next!=null){
+            if(index.contains(p1)){
+                return p1;
+            }else{
+                index.add(p1);
+            }
+            p1=p1.next;
+        }
+        return null;
+    }
+
+    public ListNode reverseList(ListNode head) {//反转链表
+        if (head==null){return null;}
+        ListNode temp,currentHead;
+        currentHead=head;
+        temp =new ListNode();
+        while(head.next!=null){
+            if(head.next.next!=null){
+                temp.next=head.next.next;
+                head.next.next=currentHead;
+                currentHead=head.next;
+                head.next=temp.next;
+            }else{
+                head.next.next=currentHead;
+                currentHead=head.next;
+                head.next=null;
+            }
+        }
+        return currentHead;
+    }
+
+    public void reverseString(char[] s) {//反转字符串，时空复杂度OK
+        int i=0;
+        int j=s.length-1;
+        char temp;
+        while(i<j){
+            temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+    }
+
+    public int findMaxConsecutiveOnes(int[] nums) {//最大连续1的个数
+        int maxCount=0;
+        int tempCount=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                tempCount++;
+                maxCount=Math.max(maxCount,tempCount);
+            }else{
+                tempCount=0;
+            }
+        }
+        return maxCount;
+    }
+
+    public int arrayPairSum(int[] nums) {//数组拆分I
+        java.util.Arrays.sort(nums);
+        int res=0;
+        for(int i=0;i<nums.length;i+=2){res+=nums[i];}
+        return res;
+    }
+
+    public int pivotIndex(int[] nums) {//寻找数组的中心索引，时间复杂度不OK
+        for(int i=0;i<=nums.length-1;++i){
+            int leftsum=0;
+            int rightsum=0;
+            for(int j=0;j<i;++j){
+                leftsum+=nums[j];
+            }
+            for(int j=i+1;j<=nums.length-1;++j){
+                if(j==nums.length)break;
+                rightsum+=nums[j];
+            }
+            if(leftsum==rightsum){return i;}
+        }
+        return -1;
     }
 }
