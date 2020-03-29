0344fea5447fb1b4e384576fc4d3b88dd1dfef18
Wallace <xpkllp@hotmail.com>
2020-03-29 09:59:12 +0800

    BinarySearch for sorted array.

--- src/edu/seu/Main.java
+++ src/edu/seu/Main.java
@@ -3,26 +3,33 @@ package edu.seu;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 //        System.out.println(Cal1.work(100));
 //        System.out.println(Cal2.work(100));
-        ListNode a=new ListNode(2);
-        ListNode s1=a;
+//        ListNode a=new ListNode(2);
+//        ListNode s1=a;
 //        a.next=new ListNode(4);
 //        a=a.next;
 //        a.next=new ListNode(3);
-        ListNode b=new ListNode(5);
-        ListNode s2=b;
+//        ListNode b=new ListNode(5);
+//        ListNode s2=b;
 //        b.next=new ListNode(6);
 //        b=b.next;
 //        b.next=new ListNode(4);
-        Solution s=new Solution();
-        ListNode result=s.addTwoNumbers(s1,s2);
-        System.out.println(result.val);
+//        Solution s=new Solution();
+//        ListNode result=s.addTwoNumbers(s1,s2);
+//        System.out.println(result.val);
 //        while(result.next!=null){
 //            System.out.println(result.val);
 //            result=result.next;
 //        }
+        int[] a={44,3,38,5,47,3,36,26,27,2,46,4,4,19,48};
+        int[] b=CustomSort.sort(a,5);
+        for (int x:b){
+            System.out.print(x+" ");
+        }
+        System.out.println();
+        System.out.println(BinarySearch.iterative(b,19));
+        System.out.println(BinarySearch.recursive(b,19,0,b.length-1));
     }
 }
 
