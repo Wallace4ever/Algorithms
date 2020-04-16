e41576421163d058cc5f1c9eac9814ce9ff24dd1
Wallace <xpkllp@hotmail.com>
2020-04-16 18:30:19 +0800

    Add Huaiwei 2020 Test : SelectCandidate. Use HashMap and ArrayList to sort wrapped items.

--- src/edu/seu/UniqueCharacter.java
+++ src/edu/seu/UniqueCharacter.java
@@ -21,3 +21,19 @@ public class UniqueCharacter {
         }
     }
 }
+
+/*public class Main{
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            char[] c = in.next().toCharArray();
+            StringBuffer sb = new StringBuffer();
+            Set<Character> set = new HashSet<Character>();
+            for(int i = 0;i<c.length;i++){
+                if(set.add(c[i]))
+                    sb.append(c[i]);
+            }
+            System.out.println(sb.toString());
+        }
+    }
+}*/
