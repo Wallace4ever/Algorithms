692a1c5e25486fbded9999580c897b4f99c23f71
wallace <xpkllp@hotmail.com>
2020-05-01 21:46:12 +0800

    二叉树前中后序遍历的递归与非递归写法，后序非递归有bug

--- src/edu/seu/Tree/RebuildTree.java
+++ src/edu/seu/Tree/RebuildTree.java
@@ -1,6 +1,7 @@
 package edu.seu.Tree;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 /**
  * 给出一个树的前序遍历和中序遍历结果，还原该二叉树
@@ -13,9 +14,14 @@ public class RebuildTree {
         TreeNode root=r.reConstructBinaryTree(pre,in);
         r.preOrder(root);
         System.out.println();
+        r.preOrder2(root);
+        System.out.println();
         r.inOrder(root);
         System.out.println();
+        r.inOrder2(root);
         r.postOrder(root);
+        System.out.println();
+//        r.postOrder2(root);
     }
     public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
         if (pre.length==0||in.length==0){
@@ -59,6 +65,65 @@ public class RebuildTree {
         if (node.right!=null)
             postOrder(node.right);
         System.out.print(node.val+" ");
+    }
+    /**
+     * 前中后序遍历的非递归写法
+     */
+    public void preOrder2(TreeNode node){
+        if (node==null) return;
+        Stack<TreeNode> s=new Stack<TreeNode>();
+        while (true){
+            while (node!=null){
+                System.out.print(node.val+" ");
+                s.push(node);
+                node=node.left;
+            }
+            if (s.empty())
+                break;
+            node=s.pop();
+            node=node.right;
+        }
+    }
+    public void inOrder2(TreeNode node){
+        if (node==null) return;
+        Stack<TreeNode> s= new Stack<>();
+        while (true){
+            while (node!=null){
+                s.push(node);
+                node=node.left;
+            }
+            if (s.empty())
+                break;
+            node=s.pop();
+            System.out.print(node.val+" ");
+            node=node.right;
+        }
+    }
+    public void postOrder2(TreeNode node){
+        // TODO: 5/1/20 写法有bug，根节点的右子树会反复出入栈
+        Stack<TreeNode> s=new Stack<>();
+        while(true){
+            if (node!=null){
+                s.push(node);
+                node=node.left;
+            }else {
+                if (s.empty()){
+                    System.out.println("Stack empty");
+                    return;
+                }else if (s.peek().right==null){
+                    node=s.pop();
+                    System.out.println(node.val+" ");
+                    if (node==s.peek().right){
+                        System.out.println(s.peek().val);
+                        s.pop();
+                    }
+                }
+                if (!s.empty())
+                    node=s.peek().right;
+                else
+                    node=null;
+            }
+        }
     }
 }
 
