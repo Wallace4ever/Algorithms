69e50311037c126387aca8dc239b14d488513073
Me#Windows <xpkllp@hotmail.com>
2020-06-04 16:47:49 +0800

    LeetCode 238 数组元素除自身外的乘积：前缀/后缀和/积的妙用。

---
+++ src/edu/LeetCode/Array/N0238_ProductOfArrayExceptSelf.java
@@ -0,0 +1,18 @@
+package edu.LeetCode.Array;
+
+public class N0238_ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result=new int[nums.length];
+        int k=1;
+        for (int i = 0; i < nums.length; i++) {
+            result[i]=k;//k临时存储当前前缀积并存在result[i]中
+            k*=nums[i];
+        }
+        k=1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result[i]*=k;//这里是已有的前缀积乘以当前后缀积k
+            k*=nums[i];
+        }
+        return result;
+    }
+}
